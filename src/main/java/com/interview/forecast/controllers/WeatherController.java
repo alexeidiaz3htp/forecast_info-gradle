27e372e86622628b0a0561a9a93fd777972c32f9
Alexei Diaz <alexei.diaz@3htp.com>
2024-04-08 13:02:17 -0400

    Add path variable to the endpoint

--- src/main/java/com/interview/forecast/controllers/WeatherController.java
+++ src/main/java/com/interview/forecast/controllers/WeatherController.java
@@ -3,14 +3,16 @@ package  com.interview.forecast.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1")
 public class WeatherController {
 
-    @GetMapping("/forecastlist")
-    public String getCityWeatherInfo(String city) {
-        String uri = "https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=q01u3w688ZDOaJKdPQ8EpUY5PkAUkeKk";
+    @GetMapping("/forecastlist/{city}")
+    public String getCityWeatherInfo(@PathParam("city") String city) {
+        String uri = "https://api.tomorrow.io/v4/weather/forecast?location=" + city + "&apikey=q01u3w688ZDOaJKdPQ8EpUY5PkAUkeKk";
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(uri, Object.class);
         return result;
