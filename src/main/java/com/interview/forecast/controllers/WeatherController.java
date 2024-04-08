92c49f698b208804bd908d28051c3c4f0dd0489c
Alexei Diaz <alexei.diaz@3htp.com>
2024-04-08 13:04:28 -0400

    Fix import PathVariable

--- src/main/java/com/interview/forecast/controllers/WeatherController.java
+++ src/main/java/com/interview/forecast/controllers/WeatherController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class WeatherController {
 
     @GetMapping("/forecastlist/{city}")
-    public String getCityWeatherInfo(@PathParam("city") String city) {
+    public Object getCityWeatherInfo(@PathVariable String city) {
         String uri = "https://api.tomorrow.io/v4/weather/forecast?location=" + city + "&apikey=q01u3w688ZDOaJKdPQ8EpUY5PkAUkeKk";
         RestTemplate restTemplate = new RestTemplate();
-        String result = restTemplate.getForObject(uri, Object.class);
+        Object result = restTemplate.getForObject(uri, Object.class);
         return result;
     }
 }
